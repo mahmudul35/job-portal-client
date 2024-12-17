b951125ec41a718834f0252c6ab3bced362b8be3
mahmudul35 <101060541+mahmudul35@users.noreply.github.com>
2024-12-17 15:55:14 +0600

    create banner component with react framer motion

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../pages/shared/Footer";
 import Navbar from "../pages/shared/Navbar";
 const MainLayout = () => {
   return (
     <div className="max-w-7xl mx-auto">
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,38 @@
+import { motion } from "motion/react";
+import React from "react";
+const Banner = () => {
+  return (
+    <div className="hero bg-base-200 min-h-96">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="flex-1">
+          <img
+            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            className="max-w-sm rounded-lg shadow-2xl"
+          />
+        </div>
+        <div className="flex-1">
+          <motion.h1
+            animate={{ x: 100, color: ["red", "green"] }}
+            transition={{
+              duration: 2,
+              delay: 1,
+              ease: "easeInOut",
+              repeat: Infinity,
+            }}
+            className="text-5xl font-bold"
+          >
+            Latest Jobs News!
+          </motion.h1>
+          <p className="py-6">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
+          <button className="btn btn-primary">Get Started</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
